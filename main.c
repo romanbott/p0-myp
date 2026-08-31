b9bc52869e4183df6343d07d164bb43e4b902d96
Roman Contreras <roman.contreras@ciencias.unam.mx>
2026-08-31 10:22:17 -0600

    feat: agrega funcion para calcular promedio

--- main.c
+++ main.c
@@ -1,17 +1,28 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
+#define NUM_CALIFICACIONES 3
 
 void mostrar_resultado(char nombre[], float promedio, bool aprobado) {
-    printf("Estudiante: %s\n", nombre);
-    printf("Promedio:   %.2f\n", promedio);
-    printf("Estado:     %s\n", aprobado ? "APROBADO" : "REPROBADO");
+  printf("Estudiante: %s\n", nombre);
+  printf("Promedio:   %.2f\n", promedio);
+  printf("Estado:     %s\n", aprobado ? "APROBADO" : "REPROBADO");
 }
 
-
+float calcular_promedio(float calificaciones[], size_t cantidad) {
+  float suma = 0.0f;
+  for (size_t i = 0; i < cantidad; i++) {
+    suma += calificaciones[i];
+  }
+  return suma / cantidad;
+}
 
 int main(void) {
-    mostrar_resultado("Juan Rulfo", 8.5, true);
+  float calificaciones[NUM_CALIFICACIONES] = {7.0, 9.0, 10.0};
 
-    return 0;
+  float promedio = calcular_promedio(calificaciones, NUM_CALIFICACIONES);
+
+  mostrar_resultado("Juan Rulfo", promedio, true);
+
+  return 0;
 }
